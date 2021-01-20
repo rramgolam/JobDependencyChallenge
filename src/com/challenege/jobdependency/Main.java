12a0e441028b81af291be8a21b6e58b5a4e1ade7
Rishi Ramgolam <33223925+rramgolam@users.noreply.github.com>
2021-01-20 14:14:52 +0000

    Initial commit

---
+++ src/com/challenege/jobdependency/Main.java
@@ -0,0 +1,8 @@
+package com.challenege.jobdependency;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ test/com/challenge/jobdependency/JobTest.java
@@ -0,0 +1,15 @@
+package com.challenge.jobdependency;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class JobTest {
+
+    @Test
+    public void createJobWithNoDependency() {
+        fail("Requires implementation.");
+    }
+
+
+}
