d51cc8e35ea23d52d9b512aab615134e24e40019
Rishi Ramgolam <33223925+rramgolam@users.noreply.github.com>
2021-01-21 14:27:35 +0000

    Two tests for first two business cases passing.

--- test/com/challenge/jobdependency/SchedulerTest.java
+++ test/com/challenge/jobdependency/SchedulerTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class SchedulerTest {
 
@@ -16,5 +17,20 @@ public class SchedulerTest {
         List<Job> result = JobScheduler.getJobSequence(jobs);
         assertEquals(1, result.size());
         assertEquals(new Job("a"), result.get(0));
+    }
+
+    @Test
+    public void testSequenceConsistingOfMultipleJobs() {
+        Job a = new Job("a");
+        Job b = new Job("b");
+        Job c = new Job("c");
+
+        List<Job> jobs = JobFactory.extractJobs("a =>,b =>,c =>");
+        List<Job> result = JobScheduler.getJobSequence(jobs);
+
+        assertEquals(a, jobs.get(0));
+        assertEquals(b, jobs.get(1));
+        assertEquals(c, jobs.get(2));
+
     }
 }
